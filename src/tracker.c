b0cd81e950de6d19d6a422ae3efa3818244c9787
Leaf <spacecats64@gmail.com>
2022-04-02 17:32:29 -0400

    get the correct pattern mmk

--- src/tracker.c
+++ src/tracker.c
@@ -79,7 +79,7 @@ uint8_t tracker_open_mod(ModTracker* tracker, char* mod){
 		if(def->sample_length == 0) continue;
 
 		tracker->module.sample_data[i] = malloc(def->sample_length);
-		fread(tracker->module.sample_data[i], def->sample_length, 1, mod_file);
+		fread(tracker->module.sample_data[i], 1, def->sample_length, mod_file);
 	}
 
 	fclose(mod_file);
@@ -110,7 +110,7 @@ void tracker_mod_tick(ModTracker* tracker){
 	if(tracker->_tick_timer >= tracker->_updates_per_tick){
 		if(tracker->_current_ticks % tracker->speed == 0){
 			for (ch = 0; ch < 4; ch++){
-				note = swap32(tracker->module.patterns[tracker->current_pattern].rows[tracker->current_row][ch]);
+				note = swap32(tracker->module.patterns[tracker->module.song_positions[tracker->current_pattern]].rows[tracker->current_row][ch]);
 				tracker->channels[ch].instrument = (note & 0xF0000000) >> 24 | (note & 0x0000F000) >> 12; 
 				tracker->channels[ch].period = (note & 0x0FFF0000) >> 16; 
 				tracker->channels[ch].volume = tracker->module.samples[tracker->channels[ch].instrument].volume;
